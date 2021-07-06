60ba1c5b9b6e846f7de94fb0ef553a3a87a243d2
NeonArtworks <flori.wagner@hotmail.com>
2021-07-06 18:04:12 +0200

    Update FarmTimer.java

--- src/main/java/at/flockenberger/bdoft/FarmTimer.java
+++ src/main/java/at/flockenberger/bdoft/FarmTimer.java
@@ -13,15 +13,15 @@ public class FarmTimer extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(FarmTimer.class.getResource("/bdoft.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(FarmTimer.class.getResource("/resources/bdoft.fxml"));
 		Parent root = fxmlLoader.load();
 		Scene scene = new Scene(root);
 
-		scene.getStylesheets().add(getClass().getResource("/systemUIDark.css").toExternalForm());
+		scene.getStylesheets().add(getClass().getResource("/resources/systemUIDark.css").toExternalForm());
 		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.setAlwaysOnTop(true);
-		stage.getIcons().add(new Image(getClass().getResource("/icon.png").toExternalForm()));
+		stage.getIcons().add(new Image(getClass().getResource("/resources/icon.png").toExternalForm()));
 		stage.setTitle("BDO Farm Timer");
 		stage.show();
 
